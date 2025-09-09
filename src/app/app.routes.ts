63132a8064119d1937ed200e66c1f8b2cf8d4078
Zapado125 <zapado125@gmail.com>
2025-09-09 13:51:04 -0300

    Enlaces
    Se creo el componente nosotros
    Se añadio el enlace a la página nosotros
    Se añadio el enlace a la página mapa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './inicio/inicio';
 import { Contacto } from './contacto/contacto';
+import { Nosotros } from './nosotros/nosotros';
+import { Mapa } from './mapa/mapa';
 
 export const routes: Routes = [
     { path: '', component: Inicio },
-    { path: 'inicio', component: Inicio }, {
-        path: 'contacto', component: Contacto
-    },
+    { path: 'inicio', component: Inicio }, 
+    { path: 'contacto', component: Contacto},
+    { path: 'nosotros', component:Nosotros},
+    { path: 'mapa', component: Mapa},
     //el resto de rutas añadir acá
 
 
