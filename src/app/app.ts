9011a1323e4d1a86ff8f9896355d88e3fd701830
Valentina <vperalta22540@gmail.com>
2025-09-09 11:47:18 -0300

    configuración inicial mapa

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Inicio } from './inicio/inicio';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Inicio],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
