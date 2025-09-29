8423468e12ad3b47011d9d9f5d4c4f2acd4a2701
Valentina <vperalta22540@gmail.com>
2025-09-29 11:30:24 -0300

    mapa configurado

---
+++ src/app/icono/icono.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icono',
+  standalone: true,
+  imports: [],
+  templateUrl: './icono.html',
+  styleUrl: './icono.css'
+})
+export class Icono {
+
+}
--- src/app/mapa/mapa.ts
+++ src/app/mapa/mapa.ts
@@ -1,22 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { MapComponent, MarkerComponent } from '@maplibre/ngx-maplibre-gl';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+
+import { Icono } from '../icono/icono';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-mapa',
-  imports: [MapComponent, MarkerComponent],
+  imports: [Icono, CommonModule],
   templateUrl: './mapa.html',
   styleUrl: './mapa.css'
 })
-export class Mapa implements OnInit {
-  userLocation: [number, number] = [-74.5, 40]; // valor por defecto (NY)
-  zoom = 14; // nivel inicial de zoom
+export class Mapa implements AfterViewInit {
+  @ViewChild('mapaContenedor', { static: true }) mapaContenedor!: ElementRef;
 
-  ngOnInit(): void {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((pos) => {
-        this.userLocation = [pos.coords.longitude, pos.coords.latitude];
-        this.zoom = 14;
+  ngAfterViewInit() {
+
+    //código mapa
+    if (typeof window !== 'undefined') {
+      import('leaflet').then(L => {
+        const map = L.map('map').setView([40.4168, -3.7038], 13); // Ej: Madrid
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '© OpenStreetMap contributors'
+        }).addTo(map);
+        
+          // Obtener la ubicación del usuario
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition((position) => {
+            const lat = position.coords.latitude;
+            const lng = position.coords.longitude;
+            // marcador con icono personalizado
+            const userIcon = L.icon({
+              iconUrl: 'https://i.ibb.co/ds5ZDWbB/Icono.png',
+              iconSize: [50, 50],
+              iconAnchor: [25, 50]
+            });
+            L.marker([lat, lng], { icon: userIcon }).addTo(map);
+            map.setView([lat, lng], 15);
+          });
+        }
       });
     }
   }
+
+  iconos: { x: number; y: number }[] = [];
+
+  agregarIcono() {
+    const contenedor = this.mapaContenedor.nativeElement;
+    const iconoSize = 50; // tamaño aproximado del icono
+    const maxX = contenedor.clientWidth - iconoSize;
+    const maxY = contenedor.clientHeight - iconoSize;
+    const x = Math.floor(Math.random() * maxX);
+    const y = Math.floor(Math.random() * maxY);
+    this.iconos.push({ x, y });
+    console.log('Iconos actuales:', this.iconos);
+  }
+
 }
