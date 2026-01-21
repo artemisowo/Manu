c1317adebac3aa95436af76c78b3e8114ff94d45
Valentina <vperalta22540@gmail.com>
2026-01-21 15:40:02 -0300

    estilos layout general
    se crearon y cambiaron los estilos generales para nosotros, contacto y donaciones.
    
    se aplicaron variables en css

--- src/app/nosotros/nosotros.ts
+++ src/app/nosotros/nosotros.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-nosotros',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nosotros.html',
   styleUrl: './nosotros.css'
 })
